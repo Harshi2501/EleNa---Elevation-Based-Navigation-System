ba4c9b626a4a331cc873f19f38e860f538da6960
Harshitha <nks.harshitha@gmail.com>
2023-05-20 23:03:15 -0400

    Updated graph_data_processor.py

--- src/NetworkMetrics/graph_data_processor.py
+++ src/NetworkMetrics/graph_data_processor.py
@@ -2,7 +2,7 @@ import osmnx as ox
 import networkx as nx
 import os
 import pickle as p
-from src.NetworkMetrics.config import API
+from config import API
 import math
 
 class graph_data_processing:
@@ -22,32 +22,53 @@ class graph_data_processing:
             self.init = False
     
     #Calculating the distance between the given latitudes and longitudes using harvesine formula
-     def harvesine_distance(self, latitude1, longitude1, latitude2, longitude2):
+     def haversine_distance(self, latitude1, longitude1, latitude2, longitude2):
          # Convert latitude and longitude from degrees to radians
         lat1_rad = math.radians(latitude1)
         lon1_rad = math.radians(longitude1)
         lat2_rad = math.radians(latitude2)
         lon2_rad = math.radians(longitude2)
 
+
+        # print(lon1_rad)
+        # print(lon2_rad)
         # Earth's radius in kilometers
         earth_radius = 6371
 
         diff_lat = lat2_rad - lat1_rad 
         diff_long = lon2_rad-lon1_rad
 
+        # print(diff_lat)
+        # print(diff_long)
         value = math.sin(diff_lat/2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(diff_long/2)**2
         temp_value = 2 * math.atan2(math.sqrt(value), math.sqrt(1-value))
-
+        
         final_value = earth_radius*temp_value
 
         #Distance in meters
         final_value_meters = final_value *1000
+        return final_value_meters
 
     #Adds elevation data to the graph
      def add_elevation_data(self, G):
          G = ox.add_node_elevations(G, elevation_key="elevation", api_key=self.GOOGLEAPIKEY)
          #Returns the updated graph with the added elevation data
          return G
+     
+def main():
+    # Test coordinates (New York City and Los Angeles)
+    lat1, lon1 = 40.7128, -74.0060
+    lat2, lon2 = 34.0522, -118.2437
+
+    obj = graph_data_processing()
+    # Compute distance using haversine_distance function
+    distance = obj.haversine_distance(lat1, lon1, lat2, lon2)
+
+    # Print the result
+    print(f"The distance between the coordinates is {distance} meters.")
+
+if __name__ == '__main__':
+    main()
      
      
     

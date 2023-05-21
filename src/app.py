ca300e63806e6d07891ae62b6512a514962d75c0
Shaunak-umass <spadhye@umass.edu>
2023-05-20 22:22:38 -0400

    Added Jquery to request route from controller

--- src/app.py
+++ src/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('map.html')
+
+@app.route('/calculate_route', methods=['POST'])
+def calculate_route():
+    waypoints = request.get_json()
+    
+    # Perform any necessary processing or calculations with the waypoints
+    
+    # Send back the same waypoints in the response
+    response = {'coordinates': waypoints}
+    
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run()

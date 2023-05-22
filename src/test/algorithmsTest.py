f3e2ad7e54b4ec99b00f1f9708a2102e2447074f
ShreyaDubey1002 <dubeyshreya1002@gmail.com>
2023-05-22 14:00:01 -0400

    Added tests to verify shortest paths and elevation calculation in routing algorithms

--- src/test/algorithmsTest.py
+++ src/test/algorithmsTest.py
@@ -1,4 +1,8 @@
-
+import networkx as nx
+import sys
+sys.path.append("..")
+from NetworkMetrics.graph_data_processor import graph_data_processing
+from RoutingAlgorithms.routing_algorithms import Algorithms
 
 class AlgorithmsTest:
 
@@ -12,25 +16,63 @@ class AlgorithmsTest:
         total_elevation = self.algorithms_obj.get_Elevation(self.route, cost_type = "elevation_difference")
         assert isinstance(total_elevation, float)
         assert total_elevation == 0.0
+        print("Test - test_elevation_elevation_difference passed")
 
     def test_elevation_gain(self):
         total_elevation = self.algorithms_obj.get_Elevation(self.route, cost_type = "elevation_gain")
         assert isinstance(total_elevation, float)
         assert total_elevation == 1.0
+        print("Test - test_elevation_gain passed")
 
     def test_elevation_drop(self):
         total_elevation = self.algorithms_obj.get_Elevation(self.route, cost_type = "elevation_drop")
         assert isinstance(total_elevation, float)
         assert total_elevation == 1.0
+        print("Test - test_elevation_drop")
 
     def test_elevation_no_elevation(self): 
         total_elevation = self.algorithms_obj.get_Elevation(self.route, cost_type = "no_elevation")
         assert isinstance(total_elevation, float)
-        print("total_elevation: " + str(total_elevation))
         assert total_elevation == 6.726999999999999
-
-    def runAllTests(self):
+        print("Test - test_elevation_no_elevation passed")   
+ 
+    def test_elevation(self):
        self.test_elevation_elevation_difference()
        self.test_elevation_gain()
        self.test_elevation_drop()
        self.test_elevation_no_elevation()
+       print("All elevation tests passed")
+
+    def test_shortest_path_maximize_elevation(self, generated_graph, source, destination, elev_perc):
+        # Test shortest path method in routing algorithms to maximize elevation
+    
+        new_algorithms_obj = Algorithms(generated_graph, elev_perc = 100.0, elev_option = "max")
+        shortest_path, best_path = new_algorithms_obj.get_shortest_path(source, destination, elev_perc, elev_option = "max", log = False)
+        assert best_path["current_distance"] <= (1 + elev_perc/100.0)*shortest_path["current_distance"]
+        assert best_path["elevation_distance"] >= shortest_path["elevation_distance"]
+        print("Test - test_shortest_path_maximize_elevation passed")
+
+    def test_shortest_path_minimize_elevation(self, generated_graph, source, destination, elev_perc):
+        # Test shortest path method in routing algorithms to minimize elevation 
+       
+        new_algorithms_obj = Algorithms(generated_graph, elev_perc = 100.0, elev_option = "min") 
+        shortest_path, best_path = new_algorithms_obj.get_shortest_path(source, destination, elev_perc, elev_option = "min", log = False)
+        assert best_path["current_distance"] <= (1 + elev_perc/100.0)*shortest_path["current_distance"]
+        assert best_path["elevation_distance"] <= shortest_path["elevation_distance"]
+        print("Test - test_shortest_path_minimize_elevation passed")
+
+    def test_shortest_path(self):
+        source = (42.3762, -72.5148)
+        destination = (42.3948, -72.5266)
+        graph_data_processing_obj = graph_data_processing()
+        generated_graph = graph_data_processing_obj.generate_graph(destination)
+        assert isinstance(generated_graph, nx.classes.multidigraph.MultiDiGraph)
+        elev_perc = 100.0
+
+        self.test_shortest_path_maximize_elevation(generated_graph, source, destination, elev_perc)
+        self.test_shortest_path_minimize_elevation(generated_graph, source, destination, elev_perc)
+        print("All shortest path tests passed")
+
+    def runAllTests(self):
+       self.test_elevation()
+       self.test_shortest_path()
